c07594d4787143e02e3f36b0f5f4906d92d38315
chandu0202 <113936980+chandu0202@users.noreply.github.com>
2022-09-22 11:41:40 +0530

    Add files via upload

---
+++ Artifact/Artifact/src/test/java/com/projectcicd/Artifact/ArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectcicd.Artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
